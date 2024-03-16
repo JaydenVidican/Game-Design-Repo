0e7cb3d5de2d81e19424b39c248c3b148f6f02dd
JaydenVidican <jevidican@stu.naperville203.org>
2024-03-15 22:53:34 -0500

    extra work and comments

--- THE Game/Assets/Scripts/CameraMovement.cs
+++ THE Game/Assets/Scripts/CameraMovement.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//honestly Im not too sure whats going on
+
 public class CameraMovement : MonoBehaviour
 {
     public Transform target;
--- THE Game/Assets/Scripts/MovementController.cs
+++ THE Game/Assets/Scripts/MovementController.cs
@@ -6,55 +6,56 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    public float speed = 5;
-    private Rigidbody2D myRigidbody;
-    private Vector3 change;
-    private Animator animator; //new
-    private bool canSprint = true;
+    public float speed = 5; //player speed
+    private Rigidbody2D myRigidbody; //creates rigidbody object
+    private Vector3 change; //creates vector object
+    private Animator animator; //creates animator object
+    private bool canSprint = true; //tells if user can sprint (if stamina added later on)
 
 
-    // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>(); //new
-        myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); //accesses animations
+        myRigidbody = GetComponent<Rigidbody2D>(); //accesses rigig body component of player
 
     }
 
-    void Update()
+    void Update() //checks every frame if player is holding spring key
     {
-        if (Input.GetKey("left shift") && canSprint == true)
+        if (Input.GetKey("left shift") && canSprint == true) 
             speed = 10;
         else
             speed = 5;
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         
-        change = Vector3.zero;
-        change.x = Input.GetAxisRaw("Horizontal");
-        change.y = Input.GetAxisRaw("Vertical");
+        change = Vector3.zero; 
+        change.x = Input.GetAxisRaw("Horizontal"); //gets horizontal movement
+        change.y = Input.GetAxisRaw("Vertical"); //gets vertical movement
         UpdateAnimationAndMove();
     }
 
-    void UpdateAnimationAndMove() //new
+    void UpdateAnimationAndMove() //moves character and updates the animation
     {
-        if(change != Vector3.zero)
+        if(change != Vector3.zero) // checks that player speed is not zero (is moving)
         {
             MoveCharacter();
-            animator.SetFloat("moveX", change.x);
+
+            //sets value for animator to display correct animation
+            animator.SetFloat("moveX", change.x); 
             animator.SetFloat("moveY", change.y);
-            animator.SetBool("moving", true); //new
+            animator.SetBool("moving", true); //transiton
         }
-        else //new
+        else 
         {
-            animator.SetBool("moving", false); //new
+            animator.SetBool("moving", false); //transiton
         }
     }
 
 
-    void MoveCharacter()
+    void MoveCharacter() //calculates player movement
     {
         myRigidbody.MovePosition(
             transform.position + change.normalized * speed * Time.deltaTime
