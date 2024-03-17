165953f876c172284584291d3894037761216ffb
JaydenVidican <jevidican@stu.naperville203.org>
2024-03-16 23:14:09 -0500

    Minimum Viable Product

--- THE Game/Assets/Scripts/LevelManager.cs
+++ THE Game/Assets/Scripts/LevelManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    private int c = 0;
     public void LoadLevel(string level)
     {
        SceneManager.LoadScene(level);
@@ -11,6 +12,13 @@ public class LevelManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void counter()
+    {
+        c++;
+        if (c == 4)
+            LoadLevel("Win");
     }
 //testing home commit
 }
