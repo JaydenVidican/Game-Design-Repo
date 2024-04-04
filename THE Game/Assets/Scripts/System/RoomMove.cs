d94f9ca342ce0598b5d512b748e6a6ff5e39497e
JaydenVidican <jevidican@stu.naperville203.org>
2024-04-04 08:30:35 -0500

    Started on house

--- THE Game/Assets/Scripts/System/RoomMove.cs
+++ THE Game/Assets/Scripts/System/RoomMove.cs
@@ -23,7 +23,7 @@ public class RoomMove : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && !other.isTrigger)
         {
             cam.minPosition = cameraMin;
             cam.maxPosition = cameraMax;
