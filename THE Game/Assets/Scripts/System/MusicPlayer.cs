55a97ed39465491703f624d5a1b39a33bb4e6cce
JaydenVidican <jevidican@stu.naperville203.org>
2024-05-03 07:59:37 -0500

    final build

--- THE Game/Assets/Scripts/System/MusicPlayer.cs
+++ THE Game/Assets/Scripts/System/MusicPlayer.cs
@@ -12,6 +12,6 @@ public class MusicPlayer : MonoBehaviour
         else
             player = this;
         
-        GameObject.DontDestroyOnLoad(gameObject); 
+        //GameObject.DontDestroyOnLoad(gameObject); 
     }
 }
